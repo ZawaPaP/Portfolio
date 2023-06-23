332537dbc677d91e8a3f3e24608cb202ada38e64
Kazuhide Izawa <kazuhide@fb.com>
2023-06-23 15:18:20 +0800

    Remove all module files

--- .next/static/NgwJYrjanMux5B_MLGNOB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-c0773b778055bddb.js","static/css/996487faf2bc0ecc.css","static/chunks/pages/index-6af4e54e6b9465fc.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
